b2c9544d43a9104110c49220ec5bdfc9f297ab31
Eduardo Guedes <eduardoguedesdev@gmail.com>
2024-08-18 01:47:56 -0300

    first commit

---
+++ src/main/java/com/eduardoguedes/monitorsever/MonitorseverApplication.java
@@ -0,0 +1,13 @@
+package com.eduardoguedes.monitorsever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorseverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorseverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduardoguedes/monitorsever/MonitorseverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardoguedes.monitorsever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorseverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
